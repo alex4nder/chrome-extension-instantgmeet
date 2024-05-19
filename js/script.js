0d6dd7eae50484e8dbd7405d2bb0db4c4d1d8191
Alexander <ag103@tadbox.com>
2024-05-19 13:41:02 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*
+ ** file: js/main.js
+ ** description: javascript code for "html/main.html" page
+ */
+
+document.getElementById("start-meeting").addEventListener("click", function () {
+  window.open("http://meet.google.com/new", "_blank");
+});
